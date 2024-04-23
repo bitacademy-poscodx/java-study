0098d08177b4b5918240dad8f7c1cf5ecd654d87
bitacademy-poscodx <poscoict.bitacademy@gmail.com>
2024-04-23 17:19:43 +0900

    practice01 #2

--- practice01/src/main/java/prob5/Prob5.java
+++ practice01/src/main/java/prob5/Prob5.java
@@ -3,5 +3,11 @@ package prob5;
 public class Prob5 {
 
 	public static void main(String[] args) {
+		int number = 89;
+		
+		String s = String.valueOf(number);
+		char c = s.charAt(1);
+		System.out.println(c);
+		
 	}
 }
