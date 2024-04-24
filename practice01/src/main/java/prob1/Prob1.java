4b7e6bdc81d64045aca6eb6951f0d4f3ecc26de8
포스코DX 청년 IT 전문가 8기 <167659749+bitacademy-poscodx@users.noreply.github.com>
2024-04-24 17:05:24 +0900

    Add files via upload

--- practice01/src/main/java/prob1/Prob1.java
+++ practice01/src/main/java/prob1/Prob1.java
@@ -6,11 +6,13 @@ public class Prob1 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner( System.in );
 		
-		System.out.print("수를 입력하세요: ");
-		
+		System.out.print("수를 입력하세요 : ");
 		int number = scanner.nextInt();
+
+		if(number % 3 == 0) {
+			System.out.println("3의 배수 입니다.");
+		}
 		
-		System.out.println(number);
 		scanner.close();
 	}
 }
