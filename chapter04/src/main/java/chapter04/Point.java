76b76027c6ba87c72580a7f78087798c129f7bb5
bitacademy-poscodx <poscoict.bitacademy@gmail.com>
2024-04-26 14:42:59 +0900

    Object #finish

--- chapter04/src/main/java/chapter04/Point.java
+++ chapter04/src/main/java/chapter04/Point.java
@@ -17,7 +17,22 @@ public class Point {
 	public int getX() {
 		return x;
 	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
 	
+	@Override
+	public String toString() {
+		return "Point [x=" + x + ", y=" + y + "]";
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(x, y);
@@ -34,19 +49,6 @@ public class Point {
 		Point other = (Point) obj;
 		return x == other.x && y == other.y;
 	}
-
-	public void setX(int x) {
-		this.x = x;
-	}
-	public int getY() {
-		return y;
-	}
-	public void setY(int y) {
-		this.y = y;
-	}
 	
-	@Override
-	public String toString() {
-		return "Point [x=" + x + ", y=" + y + "]";
-	}
+	
 }
