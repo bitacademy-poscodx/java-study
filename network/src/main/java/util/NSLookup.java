87099f2a35d119ef0b8de2bc23e3fcb54e1e8066
bitacademy-poscodx <poscoict.bitacademy@gmail.com>
2024-05-01 13:48:52 +0900

    TCPServer #1

---
+++ network/src/main/java/util/NSLookup.java
@@ -0,0 +1,19 @@
+package util;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class NSLookup {
+
+	public static void main(String[] args) {
+		try {
+			InetAddress[] inetAddresses= InetAddress.getAllByName("www.naver.com");
+			
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
